250eab2880364d9c72e5c0ceaa2ca1c877b90d7e
Marko Markovic <marko.markovic@work.co>
2017-10-25 17:39:10 +0200

    added px to rem

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-      plugins: [
-          require('postcss-import'),
-          require('postcss-mixins'),
-          require('postcss-custom-media'),
-          require('postcss-nested'),
-          require('postcss-extend'),
-      ]
+  plugins: [
+    require('postcss-import'),
+    require('postcss-mixins'),
+    require('postcss-custom-media'),
+    require('postcss-nested'),
+    require('postcss-extend'),
+    require('postcss-pxtorem'),
+  ]
 };
