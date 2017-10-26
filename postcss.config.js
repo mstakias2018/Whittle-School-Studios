b7838c4f5038d7ccdebf1e8b73f40305ed6ae111
Marko Sutija <markosutija@gmail.com>
2017-10-26 15:22:43 +0200

    core(postcss): Fix duplicates

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   plugins: [
+    require('postcss-reporter'),
     require('postcss-import'),
     require('postcss-mixins'),
+    require('postcss-discard-duplicates'),
     require('postcss-custom-media'),
     require('postcss-nested'),
     require('postcss-extend'),
     require('postcss-pxtorem'),
-  ]
+  ],
 };
