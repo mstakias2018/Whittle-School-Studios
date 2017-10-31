067fc933a05405e3837b17dee1eead0cbef790e6
Sarah Mogin <mogin@work.co>
2017-10-31 06:53:25 -0400

    fix(contentful) temp disable contentful integration (#13)

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,81 +4,78 @@ const path = require('path');
 // called after the Gatsby bootstrap is finished so you have
 // access to any information necessary to programmatically
 // create pages.
-exports.createPages = ({
-  graphql,
-  boundActionCreators,
-}) => {
-  const { createPage, } = boundActionCreators;
-
-  return new Promise((resolve, reject) => {
-    // The “graphql” function allows us to run arbitrary
-    // queries against the local Contentful graphql schema. Think of
-    // it like the site has a built-in database constructed
-    // from the fetched data that you can run queries against.
-    graphql(`
-        {
-          allContentfulArticlePage(filter: {isTopLevel: {eq: true}}) {
-            edges {
-              node {
-                id
-                slug
-                childPages {
-                  id
-                  slug
-                  childPages {
-                    id
-                    slug
-                  }
-                }
-              }
-            }
-          }
-        }
-      `
-    ).then(result => {
-      if (result.errors) {
-        reject(result.errors);
-      }
-
-      // Create Article pages
-      const articleTemplate = path.resolve('./src/templates/article/index.js');
-
-      const buildPage = (id, slugs) => {
-        // Gatsby uses Redux to manage its internal state.
-        // Plugins and sites can use functions like "createPage"
-        // to interact with Gatsby.
-
-        createPage({
-          // Each page is required to have a `path` as well
-          // as a template component. The `context` is
-          // optional but is often necessary so the template
-          // can query data specific to each page.
-          path: `/${slugs.join('/')}/`,
-          component: articleTemplate,
-          context: {
-            id
-          }
-        });
-      };
-
-      const buildPageAndChildPages = (
-        {slug, id, childPages},
-        prevSlugs = []
-      ) => {
-        const slugs = [...prevSlugs, slug];
-        buildPage(id, slugs);
-        childPages &&
-          childPages.forEach(node => {
-            buildPageAndChildPages(node, slugs);
-          });
-      };
-
-      // We want to create a detailed page for each
-      // product node. We'll just use the Contentful id for the slug.
-      result.data.allContentfulArticlePage.edges.forEach(({node}) => {
-        buildPageAndChildPages(node);
-        resolve();
-      });
-    });
-  });
+exports.createPages = ({ graphql, boundActionCreators, }) => {
+  // const { createPage, } = boundActionCreators;
+  //
+  // return new Promise((resolve, reject) => {
+  //   // The “graphql” function allows us to run arbitrary
+  //   // queries against the local Contentful graphql schema. Think of
+  //   // it like the site has a built-in database constructed
+  //   // from the fetched data that you can run queries against.
+  //   graphql(`
+  //       {
+  //         allContentfulArticlePage(filter: {isTopLevel: {eq: true}}) {
+  //           edges {
+  //             node {
+  //               id
+  //               slug
+  //               childPages {
+  //                 id
+  //                 slug
+  //                 childPages {
+  //                   id
+  //                   slug
+  //                 }
+  //               }
+  //             }
+  //           }
+  //         }
+  //       }
+  //     `
+  //   ).then(result => {
+  //     if (result.errors) {
+  //       reject(result.errors);
+  //     }
+  //
+  //     // Create Article pages
+  //     const articleTemplate = path.resolve('./src/templates/article/index.js');
+  //
+  //     const buildPage = (id, slugs) => {
+  //       // Gatsby uses Redux to manage its internal state.
+  //       // Plugins and sites can use functions like "createPage"
+  //       // to interact with Gatsby.
+  //
+  //       createPage({
+  //         // Each page is required to have a `path` as well
+  //         // as a template component. The `context` is
+  //         // optional but is often necessary so the template
+  //         // can query data specific to each page.
+  //         path: `/${slugs.join('/')}/`,
+  //         component: articleTemplate,
+  //         context: {
+  //           id
+  //         }
+  //       });
+  //     };
+  //
+  //     const buildPageAndChildPages = (
+  //       {slug, id, childPages},
+  //       prevSlugs = []
+  //     ) => {
+  //       const slugs = [...prevSlugs, slug];
+  //       buildPage(id, slugs);
+  //       childPages &&
+  //         childPages.forEach(node => {
+  //           buildPageAndChildPages(node, slugs);
+  //         });
+  //     };
+  //
+  //     // We want to create a detailed page for each
+  //     // product node. We'll just use the Contentful id for the slug.
+  //     result.data.allContentfulArticlePage.edges.forEach(({node}) => {
+  //       buildPageAndChildPages(node);
+  //       resolve();
+  //     });
+  //   });
+  // });
 };
--- src/templates/article/index.js
+++ src/templates/article/index.js
@@ -5,20 +5,21 @@ import Helmet from 'react-helmet';
 import ContentModules from '../../content-modules';
 
 const propTypes = {
-  data: PropTypes.object.isRequired
+  data: PropTypes.object.isRequired,
 };
 
 class ArticleTemplate extends React.Component {
   render() {
-    const article = this.props.data.contentfulArticlePage;
-    const {headline, modules} = article;
-    return (
-      <div>
-        <Helmet title={headline} />
-        <h1>{headline}</h1>
-        {modules && <ContentModules modules={modules} />}
-      </div>
-    );
+    // const article = this.props.data.contentfulArticlePage;
+    // const {headline, modules} = article;
+    // return (
+    //   <div>
+    //     <Helmet title={headline} />
+    //     <h1>{headline}</h1>
+    //     {modules && <ContentModules modules={modules} />}
+    //   </div>
+    // );
+    return <div>Article: under construction</div>;
   }
 }
 
@@ -26,32 +27,32 @@ ArticleTemplate.propTypes = propTypes;
 
 export default ArticleTemplate;
 
-export const pageQuery = graphql`
-  query articlePageQuery($id: String!) {
-    contentfulArticlePage(id: {eq: $id}) {
-      headline
-      modules {
-        __typename
-        ... on ContentfulInlineVideo {
-          description
-          video {
-            id
-            file {
-              url
-              fileName
-              contentType
-            }
-          }
-        }
-        ... on ContentfulBodyText {
-          content {
-            childMarkdownRemark {
-              html
-            }
-          }
-          hasDropCap
-        }
-      }
-    }
-  }
-`;
+// export const pageQuery = graphql`
+//   query articlePageQuery($id: String!) {
+//     contentfulArticlePage(id: {eq: $id}) {
+//       headline
+//       modules {
+//         __typename
+//         ... on ContentfulInlineVideo {
+//           description
+//           video {
+//             id
+//             file {
+//               url
+//               fileName
+//               contentType
+//             }
+//           }
+//         }
+//         ... on ContentfulBodyText {
+//           content {
+//             childMarkdownRemark {
+//               html
+//             }
+//           }
+//           hasDropCap
+//         }
+//       }
+//     }
+//   }
+// `;
