fd66b33cb845cb1c644c105ab72ed995cbcbd663
Marko Sutija <sutija@users.noreply.github.com>
2017-11-02 21:12:54 +0100

    add Logo img (#23)

--- src/layouts/header/header-logo/header-logo.js
+++ src/layouts/header/header-logo/header-logo.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import LogoImg from '../../../assets/images/logo.svg';
 import styles from './header-logo.module.css';
 
 const Logo = () => (
-  <div className={styles.logo} />
+  <div className={styles.logo}>
+    <img src={LogoImg} alt="Whittle school" />
+  </div>
 );
 
 export default Logo;
