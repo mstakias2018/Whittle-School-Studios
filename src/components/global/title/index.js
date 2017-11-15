6b62ebb256f50e945beff8cc32b5ebf753a38fe8
Marko Marković <marko.markovic@work.co>
2017-11-15 22:10:43 +0100

    fix(global): define vertical spacing between components (#54)

--- src/components/global/title/index.js
+++ src/components/global/title/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
 import Markdown from '../../../components/global/markdown';
 import { PROP_TYPES } from '../../../constants/custom-property-types';
@@ -11,9 +12,13 @@ const propTypes = {
   type: PROP_TYPES.PAGE_TYPES.isRequired,
 };
 
+const {
+  CLASSES,
+} = require('./../../../constants/classes');
+
 const Title = ({ text, type }) => (
   <Markdown
-    className={styles[`type${type}`]}
+    className={cx(styles[`type${type}`], CLASSES.HEADLINE)}
     containerTagName="h1"
     isTitle
     source={text}
--- src/constants/classes.js
+++ src/constants/classes.js
@@ -1,4 +1,7 @@
 exports.CLASSES = {
   PAGE_CONTENT: 'pageContent',
   FOOTER: 'footer',
+  HEADLINE: 'headline',
+  SHARE_COMPONENT: 'shareComponent',
+  SECTION_TITLE: 'sectionTitle'
 };
