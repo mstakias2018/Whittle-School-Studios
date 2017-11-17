9c7640b37dec18f0219f13c50edc1334e4ed8be4
Sarah Mogin <mogin@work.co>
2017-11-17 04:11:23 -0500

    fix(deploy) Copy China dev to US dev before copying dev spaces to QA (#60)

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,5 @@
-import { exec } from 'child_process';
+/* eslint-disable no-console */
+import simpleGit from 'simple-git';
 
 import transferSpaces from './transfer-spaces';
 
@@ -9,7 +10,14 @@ const US_DEV = 'oddfnk02van2';
 const CHINA_QA = '5wvb725fovi7';
 const US_QA = 'w6h2a8mys1um';
 
-transferSpaces(CHINA_DEV, CHINA_QA);
-transferSpaces(US_DEV, US_QA);
-
-exec('git push origin master:qa');
+console.log('=== COPYING CHINA DEV TO US DEV');
+transferSpaces(CHINA_DEV, US_DEV, true).then(() => {
+  console.log('=== COPYING US DEV TO US QA');
+  transferSpaces(US_DEV, US_QA).then(() => {
+    console.log('=== COPYING CHINA DEV TO CHINA QA');
+    transferSpaces(CHINA_DEV, CHINA_QA).then(() => {
+      console.log('=== PUSHING TO QA BRANCH');
+      simpleGit().fetch().push('origin', 'refs/remotes/origin/master:refs/heads/qa');
+    });
+  });
+});
--- scripts/transfer-spaces.js
+++ scripts/transfer-spaces.js
@@ -6,39 +6,45 @@ import spaceImport from 'contentful-import';
 // TODO ENV VARS
 const MANAGEMENT_TOKEN = 'CFPAT-d3337097476df5ed2ab54c9764486916b6c9e851ccffc075eb7f70282e9ea6f3';
 
-export default (source, target, isEnglishOnly = false) => spaceExport({
-  managementToken: MANAGEMENT_TOKEN,
-  saveFile: false,
-  skipWebhooks: true,
-  spaceId: source,
-}).then((output) => {
-  const { locales, entries } = output;
-  console.log('Export space data:', output);
-  const content = isEnglishOnly ? {
-    ...output,
-    entries: entries.map((entry) => {
-      const fieldNames = entry.fields ? Object.keys(entry.fields) : [];
-      return {
-        ...entry,
-        fields: fieldNames.reduce((acc, fieldName) => {
-          acc[fieldName] = { 'en-US': entry.fields[fieldName]['en-US'] };
-          return acc;
-        }, {}),
-      };
-    }),
-    locales: locales.filter(({ code }) => code === 'en-US'),
-  } : output;
-  spaceImport({
-    content,
-    managementToken: MANAGEMENT_TOKEN,
-    spaceId: target,
-  }).then(() => {
-    console.log('Data imported successfully');
-  })
-    .catch((err) => {
-      console.log('Import error', err);
-    });
-})
-  .catch((err) => {
-    console.log('Export error', err);
+const filterFields = entries =>
+  entries.map((entry) => {
+    const fieldNames = entry.fields ? Object.keys(entry.fields) : [];
+    return {
+      ...entry,
+      fields: fieldNames.reduce((acc, fieldName) => {
+        acc[fieldName] = { 'en-US': entry.fields[fieldName]['en-US'] };
+        return acc;
+      }, {}),
+    };
   });
+
+export default (source, target, shouldFilterNonEnglish = false) =>
+  new Promise(resolve =>
+    spaceExport({
+      managementToken: MANAGEMENT_TOKEN,
+      saveFile: false,
+      skipWebhooks: true,
+      spaceId: source,
+    }).then((output) => {
+      const { assets, entries, locales } = output;
+      const content = shouldFilterNonEnglish ? {
+        ...output,
+        assets: filterFields(assets),
+        entries: filterFields(entries),
+        locales: locales.filter(({ code }) => code === 'en-US'),
+      } : output;
+      spaceImport({
+        content,
+        managementToken: MANAGEMENT_TOKEN,
+        spaceId: target,
+      }).then(() => {
+        console.log('Data imported successfully');
+        resolve();
+      })
+        .catch((err) => {
+          console.log('Import error', err);
+        });
+    })
+      .catch((err) => {
+        console.log('Export error', err);
+      }));
