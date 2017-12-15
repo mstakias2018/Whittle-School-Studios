c1da38f6a7402e65d2301d39ab0109a11baddef5
Sarah Mogin <mogin@work.co>
2017-12-15 05:55:25 -0500

    fix(Media query) Switch from em to px image media queries for Safari bug (#185)

--- src/constants/images.js
+++ src/constants/images.js
@@ -16,19 +16,15 @@ const IMAGE_SIZE = {
 
 exports.IMAGE_SIZE = IMAGE_SIZE;
 
+/* We would prefer to use em but switching back to px because of this Safari bug
+   https://bugs.webkit.org/show_bug.cgi?id=159635 */
 exports.IMAGE_MQ = {
-  /* 375px */
-  [IMAGE_SIZE.SMALL]: '(max-width: 23.4375rem)',
-  /* 650px */
-  [IMAGE_SIZE.SMALL_MID]: '(max-width: 40.625em)',
-  /* 872px */
-  [IMAGE_SIZE.MEDIUM]: '(max-width: 54.5em)',
-  /* 1100px */
-  [IMAGE_SIZE.MEDIUM_MID]: '(max-width: 68.75em)',
-  /* 1280px */
-  [IMAGE_SIZE.LARGE]: '(max-width: 80em)',
-  /* 1280px */
-  [IMAGE_SIZE.LARGE_MID]: '(min-width: 80em)',
+  [IMAGE_SIZE.SMALL]: '(max-width: 375px)',
+  [IMAGE_SIZE.SMALL_MID]: '(max-width: 650px)',
+  [IMAGE_SIZE.MEDIUM]: '(max-width: 872px)',
+  [IMAGE_SIZE.MEDIUM_MID]: '(max-width: 1100px)',
+  [IMAGE_SIZE.LARGE]: '(max-width: 1280px)',
+  [IMAGE_SIZE.LARGE_MID]: '(min-width: 1280px',
 };
 
 /* IMAGE CONFIG */
