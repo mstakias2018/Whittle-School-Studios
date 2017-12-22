450cf07994be0f646e62ce769e6e39ec0816d009
Marko Stankovic <marko.stankovic@work.co>
2017-12-22 17:08:44 +0100

    feat(inquiry-form): Add loading placeholder (#218)
    * feat(inquiry-form): Add loading placeholder
    
    Closes ticket #1022
    
    * svg remove the unnecessary content
    
    * remove loader after iframe is fully loaded

--- src/content-modules/global/openapply-iframe/index.js
+++ src/content-modules/global/openapply-iframe/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import cx from 'classnames';
 
 import Markdown from '../../../components/global/markdown';
 import styles from './openapply-iframe.module.css';
 import { PROP_SHAPES } from '../../../constants/custom-property-types';
+
+import loadingSpinner from '../../../assets/images/loading-spinner.svg';
 
 const propTypes = {
   description: PROP_SHAPES.MARKDOWN.isRequired,
@@ -14,10 +17,41 @@ const propTypes = {
 class OpenApplyIFrame extends Component {
   state = {
     hasMounted: false,
+    iframeHeight: 0,
+    isIframeLoaded: false,
   }
 
   componentDidMount() {
     this.setState({ hasMounted: true });
+    this.observer = new MutationObserver(this.onChange);
+
+    if (this.wrapper) {
+      this.observer.observe(this.wrapper, { attributes: true, childList: true });
+    }
+  }
+
+  componentWillUnmount() {
+    this.observer.disconnect();
+  }
+
+  onChange = (elements) => {
+    elements.forEach((el) => {
+      if (el.addedNodes[0] && el.addedNodes[0].nodeName.toUpperCase() === 'IFRAME') {
+        this.observer.observe(el.addedNodes[0], { attributes: true });
+
+        this.setState({
+          iframeHeight: el.addedNodes[0].offsetHeight,
+        });
+      }
+
+      if (el.attributeName === 'style' && el.target.nodeName.toUpperCase() === 'IFRAME') {
+        if (el.target.offsetHeight !== this.state.iframeHeight) {
+          this.setState({
+            isIframeLoaded: true,
+          });
+        }
+      }
+    });
   }
 
   render() {
@@ -34,11 +68,26 @@ class OpenApplyIFrame extends Component {
           }
         />
         <div className={styles.componentWrapper}>
-          <div className={styles.componentContent}>
+          <div
+            className={cx(
+              styles.componentContent,
+              {
+                [styles.iframe_loaded]: this.state.isIframeLoaded,
+              },
+            )}
+            ref={(el) => { this.wrapper = el; }}
+          >
             <Markdown
               className={styles.description}
               source={description.markdown}
             />
+            <div className={styles.loadingPlaceholder}>
+              <img
+                alt=""
+                className={styles.loadingSpinner}
+                src={loadingSpinner}
+              />
+            </div>
             <span id="openapplyScript" />
           </div>
         </div>
