e7d35bb9c95307f49e6ea8f73c8c4e15a9d00841
Marko Stankovic <marko.stankovic@work.co>
2018-01-10 11:55:02 +0100

    fix(hero): Prevent from Build fail if link not exist (#272)
    Closes ticket #1730

--- src/content-modules/global/hero/schools-intro/index.js
+++ src/content-modules/global/hero/schools-intro/index.js
@@ -20,53 +20,45 @@ const SchoolsIntro = ({
   title,
   countdownTitle,
   countdownDate,
-}) => {
-  const {
-    linkDestinationExternal,
-    linkDestinationInternal,
-    linkTitle,
-  } = link;
-
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.introWrapper}>
-        <Picture
-          alt={imageAlt}
-          className={styles.image}
-          sourcesBySize={image}
-        />
-        <div className={styles.content}>
-          <div className={styles.contentWrapper}>
-            <div className={styles.contentInner}>
-              <h2 className={styles.title}>{title}</h2>
-              <p className={styles.description}>{description.markdown}</p>
-              {(linkDestinationInternal || linkDestinationExternal) &&
-              <Link
-                className={styles.link}
-                to={parseLink({ external: linkDestinationExternal, internal: linkDestinationInternal })}
-              >
-                {linkTitle}
-              </Link>
-              }
-            </div>
-          </div>
-          <div className={cx(styles.countdownWrapper, 'showMd')}>
-            <OpeningCountdown
-              date={countdownDate}
-              title={countdownTitle}
-            />
+}) => (
+  <div className={styles.wrapper}>
+    <div className={styles.introWrapper}>
+      <Picture
+        alt={imageAlt}
+        className={styles.image}
+        sourcesBySize={image}
+      />
+      <div className={styles.content}>
+        <div className={styles.contentWrapper}>
+          <div className={styles.contentInner}>
+            <h2 className={styles.title}>{title}</h2>
+            <p className={styles.description}>{description.markdown}</p>
+            {link && (link.linkDestinationInternal || link.linkDestinationExternal) &&
+            <Link
+              className={styles.link}
+              to={parseLink({ external: link.linkDestinationExternal, internal: link.linkDestinationInternal })}
+            >
+              {link.linkTitle}
+            </Link>
+            }
           </div>
         </div>
-      </div>
-      <div className={cx(styles.countdownWrapper, 'hideMd')}>
-        <OpeningCountdown
-          date={countdownDate}
-          title={countdownTitle}
-        />
+        <div className={cx(styles.countdownWrapper, 'showMd')}>
+          <OpeningCountdown
+            date={countdownDate}
+            title={countdownTitle}
+          />
+        </div>
       </div>
     </div>
-  );
-};
+    <div className={cx(styles.countdownWrapper, 'hideMd')}>
+      <OpeningCountdown
+        date={countdownDate}
+        title={countdownTitle}
+      />
+    </div>
+  </div>
+);
 
 SchoolsIntro.propTypes = propTypes;
 
