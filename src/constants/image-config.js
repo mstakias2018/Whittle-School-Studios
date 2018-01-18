8083bc8fc64e600a35bc2412108f8bf835fdc6f2
Marko Stankovic <marko.stankovic@work.co>
2018-01-18 13:22:46 +0100

    fix(videos): Fix videos module image sizes (#326)
    Closes ticket #1997

--- src/constants/image-config.js
+++ src/constants/image-config.js
@@ -31,7 +31,7 @@ const IMAGE_CONFIG = {
   [IMAGE_SUBTYPE.INLINE_RT_HERO_VIDEO]: {
     [BREAKPOINTS_NAME.small]: { colRatio: 1, heightRatio: RATIO.RECT },
     [BREAKPOINTS_NAME.medium]: { colRatio: 1, heightRatio: RATIO.RECT },
-    [BREAKPOINTS_NAME.large]: { colRatio: 6 / 12, heightRatio: RATIO.RECT },
+    [BREAKPOINTS_NAME.large]: { colRatio: 8 / 12, heightRatio: RATIO.RECT },
   },
   [IMAGE_SUBTYPE.INLINE_RT_HERO_VIDEO_SMALL]: {
     [BREAKPOINTS_NAME.small]: { colRatio: 2 / 4, heightRatio: RATIO.RECT },
